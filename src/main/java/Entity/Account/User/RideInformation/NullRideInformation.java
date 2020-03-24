c6230e5dc221b4db4f17544475c9c6af866f9ebc
Gyucheon Heo <gyucheon.heo@gmail.com>
2020-03-24 17:58:49 -0500

    Add RideInformation

---
+++ src/main/java/Entity/Account/User/RideInformation/NullRideInformation.java
@@ -0,0 +1,8 @@
+package Entity.Account.User.RideInformation;
+
+public class NullRideInformation extends RideInformation {
+    @Override
+    public boolean isNil(){
+        return true;
+    }
+}
---
+++ src/test/java/RideInformationTest.java
@@ -0,0 +1,38 @@
+import Entity.Account.User.RideInformation.RideInformation;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RideInformationTest {
+    private RideInformation rInfo;
+    @Before
+    public void setUp(){
+        rInfo = new RideInformation();
+    }
+
+    @Test
+    public void whenJustCreateUser_Driver_should_not_be_null(){
+        Assert.assertNotNull(rInfo);
+    }
+    @Test
+    public void whenJustCreateUser_rides_should_be_zero(){
+        Assert.assertEquals(0, rInfo.getRides());
+    }
+    @Test
+    public void whenJustCreateUser_average_ratings_should_be_zero(){
+        Assert.assertEquals(0.0, rInfo.getAverageRating(), 0.001);
+    }
+    @Test
+    public void rides_should_not_be_negative(){
+        Assert.assertTrue(rInfo.getRides() >= 0);
+    }
+    @Test
+    public void average_ratings_should_not_be_negative(){
+        Assert.assertTrue(rInfo.getAverageRating() >=0);
+    }
+    @Test
+    public void ratings_should_be_equal_to_size_of_rate(){
+        Assert.assertEquals(rInfo.getRatings(), rInfo.getRates().size());
+    }
+
+}
