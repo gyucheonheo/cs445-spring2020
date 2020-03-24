e101699b82b2e473abb803fe12cec5b672fe2cdf
Gyucheon Heo <gyucheon.heo@gmail.com>
2020-03-24 18:06:27 -0500

    Update CellPhoneFormat

--- src/main/java/Entity/Account/CellPhoneFormat/CellPhoneFormat.java
+++ src/main/java/Entity/Account/CellPhoneFormat/CellPhoneFormat.java
@@ -1,49 +1,29 @@
 package Entity.Account.CellPhoneFormat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.lang.Character.*;
 
 public class CellPhoneFormat {
-    private String first;
-    private String middle;
-    private String last;
+    final Map<String, String> VALUES_BY_NAME;
 
     public CellPhoneFormat(String first, String middle, String last){
-        setFirst(first);
-        setMiddle(middle);
-        setLast(last);
+        VALUES_BY_NAME = new HashMap<>();
+        setElement("first", first, 3, new FirstMustBeThreeDigit(), new FirstMustBeDigit());
+        setElement("middle", middle, 3, new MiddleMustBeThreeDigit(), new MiddleMustBeDigit());
+        setElement("last", last, 4, new LastMustBeFourDigit(), new LastMustBeDigit());
     }
-        private void setFirst(String first){
-            if(first.length() != 3){
-                throw new FirstMustBeThreeDigit();
+        private void setElement(String key, String value, int limit, RuntimeException e1, RuntimeException e2){
+            if(value.length() != limit){
+                throw e1;
             }
-            for( char c : first.toCharArray()){
-               if( !isDigit(c) ){
-                   throw new FirstMustBeDigit();
-               }
-
-            }
-        }
-        private void setMiddle(String middle){
-            if(middle.length() != 3){
-                throw new MiddleMustBeThreeDigit();
-            }
-            for( char c : middle.toCharArray()){
-                if( !isDigit(c) ){
-                    throw new MiddleMustBeDigit();
+            for( char c : value.toCharArray()){
+                if( !isDigit(c)){
+                    throw e2;
                 }
-
             }
-        }
-        private void setLast(String last){
-            if(last.length() != 4){
-                throw new LastMustBeFourDigit();
-            }
-            for( char c : last.toCharArray()){
-                if( !isDigit(c) ){
-                    throw new LastMustBeDigit();
-                }
-
-            }
+            this.VALUES_BY_NAME.put(key, value);
         }
         public static class FirstMustBeThreeDigit extends RuntimeException{}
         public static class FirstMustBeDigit extends RuntimeException{}
@@ -51,5 +31,14 @@ public class CellPhoneFormat {
         public static class MiddleMustBeDigit extends RuntimeException{}
         public static class LastMustBeFourDigit extends RuntimeException{}
         public static class LastMustBeDigit extends RuntimeException{}
+    public String getFirst(){
+        return VALUES_BY_NAME.get("first");
+    }
+    public String getMiddle(){
+        return VALUES_BY_NAME.get("middle");
+    }
+    public String getLast(){
+        return VALUES_BY_NAME.get("last");
+    }
 }
 
