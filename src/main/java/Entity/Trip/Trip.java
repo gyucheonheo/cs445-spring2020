877c27b5a4e849a50fdcdf4aa8c78879ccfe6c4a
Gyucheon Heo <gyucheon.heo@gmail.com>
2020-03-24 20:29:05 -0500

    Update Trip members(But need to more reduce parameters)

--- src/main/java/Entity/Trip/Trip.java
+++ src/main/java/Entity/Trip/Trip.java
@@ -1,23 +1,28 @@
 package Entity.Trip;
 
+import Entity.Trip.Car.Car;
 import Entity.Trip.Condition.Condition;
+import Entity.Trip.DateTimeFormat.DateTimeFormat;
+import Entity.Trip.LocationInformation.LocationInformation;
 import Lib.UniqueId;
 
-import java.util.Date;
 import java.util.List;
 
 public class Trip {
     private String tid;
-    private Location startingPoint;
-    private Location endingPoint;
-    private Date departureTime;
+    private LocationInformation locationInformation;
+    private DateTimeFormat dateTimeInformation;
+    private Car carInformation;
     private int maxPeople;
-    private int fee = 0;
-    // I could use Decoration Design Pattern for conditions.
+    private double amount_per_passenger = 0;
     private List<Condition> conditions;
 
     public Trip(){
+
+    }
+    public Trip(LocationInformation locationInformation, DateTimeFormat dateInformation, Car car, int maxPeople, double amount_per_passenger, List<Condition> conditions){
         this.tid = UniqueId.getUniqueID();
+
     }
 
     public boolean isNil(){
