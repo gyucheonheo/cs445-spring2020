6a1efcd1efbd227180bb89d22320e4ececc28a64
Gyucheon Heo <gyucheon.heo@gmail.com>
2020-04-01 23:37:57 -0500

    Add useful libraries

--- src/main/java/Lib/UniqueId.java
+++
@@ -1,9 +0,0 @@
-package Lib;
-import java.util.concurrent.atomic.AtomicInteger;
-
-public class UniqueId {
-    private static AtomicInteger atomicInteger = new AtomicInteger();
-    public static String getUniqueID() {
-        return Integer.toString(atomicInteger.incrementAndGet());
-    }
-}
