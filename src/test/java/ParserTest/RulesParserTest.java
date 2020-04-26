2f9cd1f761319e893314762472f228902cb9fb76
Gyucheon Heo <gyucheon.heo@gmail.com>
2020-04-26 10:27:14 -0500

    Change RulesParserTest

--- src/test/java/ParserTest/RulesParserTest.java
+++ src/test/java/ParserTest/RulesParserTest.java
@@ -9,10 +9,10 @@ import org.junit.Test;
 
 public class RulesParserTest {
     @Test
-    public void rule_parse_and_return_rule_object_null_case(){
+    public void rule_parse_and_return_rule_zero_case(){
         Rules r = RulesParser.parse(5, JsonNull.INSTANCE, "No Smoking");
         Assert.assertEquals(5, r.getMaxPeople());
-        Assert.assertNull(r.getAmountPerPassenger());
+        Assert.assertEquals(0.0,r.getAmountPerPassenger());
         Assert.assertEquals("No Smoking", r.getConditions());
     }
 
